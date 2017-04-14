5d6cd4fa093b55fe79b354fd03ac6ea78c7ef981
Arash Saffari <arash@saffari.com>
2017-04-14 21:21:03 +0200

    Change to JUST HTML

--- html5_bootstrap_contao/files/as_template/general/css/sass_compiler/compile_scss.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require "./lib/php-sass.php";
-$sass_watcher = new SassWatcher("./lib/scss.inc.php");
-$sass_watcher->compile_scss("../scss/", "../", 1, "scss_formatter_compressed");
-
-?>
--- html5_bootstrap_laravel/public/files/general/css/sass_compiler/watch_scss.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require "./lib/php-sass.php";
-$sass_watcher = new SassWatcher("./lib/scss.inc.php");
-$sass_watcher->watch("../scss/", "../", 2, "scss_formatter_compressed");
-
-?>
