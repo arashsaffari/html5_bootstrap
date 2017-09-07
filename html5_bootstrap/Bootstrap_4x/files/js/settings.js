c36fda619ee7d9a84be96aecea002170caa0a323
Arash Saffari <arash@saffari.com>
2017-09-07 03:33:01 +0200

    SCSS and Bootstrap 4.0.0 Beta

---
+++ html5_bootstrap/Bootstrap_4x/files/js/settings.js
@@ -0,0 +1,17 @@
+jQuery(function($){
+
+	var myApp = window.myApp || {};
+
+
+	myApp.menu_hover = function(){
+
+		$(document).ready(function() {
+			
+		});
+	}
+
+
+	//init
+	myApp.menu_hover();
+
+});
