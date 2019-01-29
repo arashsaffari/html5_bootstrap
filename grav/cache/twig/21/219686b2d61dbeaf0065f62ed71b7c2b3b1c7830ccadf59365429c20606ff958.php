4b06a4dd281fa45a73720ea038befbb688e7aea4
Arash Saffari <arash@saffari.com>
2019-01-29 02:28:00 +0100

    add

---
+++ grav/cache/twig/21/219686b2d61dbeaf0065f62ed71b7c2b3b1c7830ccadf59365429c20606ff958.php
@@ -0,0 +1,42 @@
+<?php
+
+/* @Page:/Users/arashsaffari/Datein/Web Server/htdocs/html5_bootstrap/grav/user/pages/02.elements/03.Subs/05.sub-1-5/02.sub-1-5-2/01.sub-1-5-2-1 */
+class __TwigTemplate_23930aa5cbd10eba2a59f48084266c804786a70609b6ff9492d83714ec607ff2 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+    }
+
+    public function getTemplateName()
+    {
+        return "@Page:/Users/arashsaffari/Datein/Web Server/htdocs/html5_bootstrap/grav/user/pages/02.elements/03.Subs/05.sub-1-5/02.sub-1-5-2/01.sub-1-5-2-1";
+    }
+
+    public function getDebugInfo()
+    {
+        return array ();
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "@Page:/Users/arashsaffari/Datein/Web Server/htdocs/html5_bootstrap/grav/user/pages/02.elements/03.Subs/05.sub-1-5/02.sub-1-5-2/01.sub-1-5-2-1", "");
+    }
+}
