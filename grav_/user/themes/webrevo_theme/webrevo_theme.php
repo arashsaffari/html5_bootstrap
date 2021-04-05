f3e3390db22cf0d3ec58e80ccd0c8a8912cff4fa
Arash Saffari <arash@saffari.com>
2021-04-05 22:27:39 +0200

    add

--- grav_/user/themes/webrevo_theme/webrevo_theme.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Grav\Theme;
-
-use Grav\Common\Theme;
-
-class Webrevo_theme extends Theme
-{
-
-}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'be12aa088454a1207208bba542bdb33258ec375c',
+    'name' => 'arassaffati/html5_bootstrap',
+  ),
+  'versions' => 
+  array (
+    'arassaffati/html5_bootstrap' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'be12aa088454a1207208bba542bdb33258ec375c',
+    ),
+  ),
+);
